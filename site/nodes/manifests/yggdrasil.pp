aa414aaca59b4e34fad2a4bfa07be3fe73729ae9
Adrien Thebo <git@somethingsinistral.net>
2015-08-14 12:55:00 -0700

    yggdrasil: add default server

--- site/nodes/manifests/yggdrasil.pp
+++ site/nodes/manifests/yggdrasil.pp
@@ -28,4 +28,28 @@ class nodes::yggdrasil {
     www_root => '/srv/www/somethingsinistral.net',
     require  => File['/srv/www/somethingsinistral.net'],
   }
+
+  # Add dead default vhost
+
+  file { '/srv/www/default_vhost':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/srv/www/default_vhost/index.html':
+    ensure  => file,
+    content => "Go away.\n",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  nginx::resource::vhost { 'default_vhost':
+    listen_options => 'default_server',
+    server_name    => ['_'],
+    www_root       => '/srv/www/default_vhost',
+    require        => File['/srv/www/default_vhost'],
+  }
 }
